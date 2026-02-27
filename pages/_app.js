e736c9817e1e5c694accb4a0321924772127b1d0
Elya <elyaallahyari@gmail.com>
2026-02-27 23:33:18 +0330

    Set Persian Font

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,38 @@
 import '@/styles/globals.css'
 import Layout from '@/pages/layout'
+import localFont from 'next/font/local'
+
+const farsiFont = localFont({
+  src: [
+    {
+      path: '../public/fonts/PlaypenSansArabic-Regular.woff',
+      weight: '400'
+    },
+    {
+      path: '../public/fonts/PlaypenSansArabic-Medium.woff',
+      weight: '500'
+    },
+    {
+      path: '../public/fonts/PlaypenSansArabic-SemiBold.woff',
+      weight: '600'
+    },
+    {
+      path: '../public/fonts/PlaypenSansArabic-Bold.woff',
+      weight: '700'
+    },
+    {
+      path: '../public/fonts/PlaypenSansArabic-ExtraBold.woff',
+      weight: '800'
+    }
+  ]
+})
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={farsiFont.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   )
 }
