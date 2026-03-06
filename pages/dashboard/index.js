6b91ca697cc3ae763fa159737390d8373c0685ea
Elya <elyaallahyari@gmail.com>
2026-03-06 20:20:34 +0330

    Navigate Add-Button

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,14 @@
 import ProductList from '@/components/product-list'
 import DashboardLayout from '@/pages/dashboard/_layouts/dashboardLayout'
 import AddButton from '@/ui/add-button'
+import Link from 'next/link'
 
 export default function Dashboard() {
   return (
     <div className="flex flex-col justify-start items-center w-full h-screen p-3">
-      <AddButton />
+      <Link href={'/dashboard/product/add'}>
+        <AddButton />
+      </Link>
       <ProductList />
     </div>
   )
