3afd8bbadff4f8b87024c4e24a58d3ccbda3b767
Elya <elyaallahyari@gmail.com>
2026-04-27 12:19:32 +0330

    Update icon

--- ui/add-button.js
+++ ui/add-button.js
@@ -1,7 +1,9 @@
+import { GrFormAdd } from 'react-icons/gr'
+
 export default function AddButton() {
   return (
-    <button className="bg-green-600 hover:bg-green-700 px-4 py-2 rounded-lg text-2xl absolute top-24 right-10 cursor-pointer">
-      +
+    <button className="bg-green-600 hover:bg-green-700 p-2 rounded-lg text-2xl absolute top-24 right-10 cursor-pointer">
+      <GrFormAdd />
     </button>
   )
 }
