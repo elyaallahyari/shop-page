57471cd5fd6a8be8ee46171d2ca75d35e7efeb8e
Elya <elyaallahyari@gmail.com>
2026-04-27 12:11:30 +0330

    Update icon

--- components/sidebar-dashboard.js
+++ components/sidebar-dashboard.js
@@ -1,18 +1,25 @@
 import Link from 'next/link'
+import { TbHome } from 'react-icons/tb'
 
 export default function DashboardSidebar({ isVisible }) {
   return (
     <div className={`${isVisible ? 'block' : 'hidden'}`}>
       <aside className="flex-col justify-start items-center text-left h-full w-full max-w-50 bg-gray-100 pt-10 z-50 p-3">
         <ul className="flex flex-col gap-10 items-center ltr">
-          <li className="menu_item">
-            <Link href={'/dashboard'}>Home 🏠</Link>
+          <li className="menu_li">
+            <Link href={'/dashboard'} className="menu_link">
+              Home <TbHome />
+            </Link>
           </li>
-          <li className="menu_item">
-            <Link href={'/dashboard/media'}>Media 📷</Link>
+          <li className="menu_li">
+            <Link href={'/dashboard/media'} className="menu_link">
+              Media 📷
+            </Link>
           </li>
-          <li className="menu_item">
-            <Link href={'/dashboard/product/add'}>Add item ➕</Link>
+          <li className="menu_li">
+            <Link href={'/dashboard/product/add'} className="menu_link">
+              Add item ➕
+            </Link>
           </li>
         </ul>
       </aside>
