020c4fbe095f1996ec966c791fd107b52079979f
Elya <elyaallahyari@gmail.com>
2026-04-27 12:00:27 +0330

    Update icon

--- ui/menu-button.js
+++ ui/menu-button.js
@@ -1,10 +1,11 @@
+import { BsMenuButtonFill } from 'react-icons/bs'
 export default function MenuButton({ toggleMenu }) {
   return (
     <button
       onClick={toggleMenu}
       className="bg-gray-300 hover:bg-gray-200 rounded-lg px-4 py-2 cursor-pointer absolute right-6 bottom-6 shadow z-10"
     >
-      ⑆
+      <BsMenuButtonFill />
     </button>
   )
 }
